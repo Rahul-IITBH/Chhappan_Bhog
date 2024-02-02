8a6c91ab52d3ec826582d983ba2ac48c965ab073
“Rahul-IITBH” <rahulkumarjaiswal351@gmail.com>
2024-02-03 00:19:57 +0530

    Updated

--- Chhappan_Bhog/settings.py
+++ Chhappan_Bhog/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = "Chhappan_Bhog.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR + "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Food_Alicious',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
